c40c6a23ec9fdcdb20b06e12a0635510ce907153
小白 <1140279968@qq.com>
2021-02-24 11:13:30 +0800

    feat: 🎸 一个简单的event测试demo
    一个简单的event测试demo

---
+++ src/components/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<button @click="handleClick">Increment</button>
+		<button @click="decrement">decrement</button>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    data() {
+        return {
+            count: 0
+        }
+    },
+    emits: ['increment', 'decrement'],
+    methods: {
+        handleClick() {
+            this.count += 1
+            this.$emit('increment', this.count)
+        },
+        decrement() {
+            this.count -= 1
+            this.$emit('decrement', {
+                count: this.count,
+                isEven: this.count % 2 === 0
+            })
+        }
+    }
+})
+</script>
