58783643444ca5961471af6afe6ecb7ec2c27deb
小白 <1140279968@qq.com>
2021-02-24 16:34:07 +0800

    feat: 🎸 完成form表单的验证
    完成form表单的验证

---
+++ src/components/ExtraForm.vue
@@ -0,0 +1,25 @@
+<template>
+	<form>
+		<input type="text" name="" v-model="value" @blur="handleBlur" />
+		<button>Submit</button>
+	</form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name: 'Form',
+    data() {
+        return {
+            value: ''
+        }
+    },
+    methods: {
+        handleBlur(event:MouseEvent) {
+            if ((event.relatedTarget as any).tagName === 'BUTTON') {
+                this.$emit('focus-lost')
+            }
+        }
+    }
+})
+</script>
