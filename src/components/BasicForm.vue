64d34707d848e12d07b0af5e0d14298bae8fceb6
小白 <1140279968@qq.com>
2021-02-24 14:55:47 +0800

    feat: 🎸 form表单的test测试
    form表单的test测试

---
+++ src/components/BasicForm.vue
@@ -0,0 +1,38 @@
+<template>
+	<form @submit.prevent="submit">
+		<input type="email" v-model="form.email" />
+		<textarea name="" id="" cols="30" rows="10" v-model="form.description"></textarea>
+		<select v-model="form.city">
+			<option value="new-york">New York</option>
+			<option value="moscow">Moscow</option>
+		</select>
+		<input type="checkbox" v-model="form.subscribe" />
+		<input type="radio" value="weekly" v-model="form.interval" />
+		<input type="radio" value="monthly" v-model="form.interval" />
+
+		<button @click="submit">Submit</button>
+	</form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    data() {
+        return {
+            form: {
+                email: '',
+                description: '',
+                city: '',
+                subscribe: false,
+                interval: ''
+            }
+        }
+    },
+    emits: ['submit'],
+    methods: {
+        submit() {
+            this.$emit('submit', this.form)
+        }
+    }
+})
+</script>
